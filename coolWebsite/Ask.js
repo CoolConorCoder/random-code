f623400e6b06417c8f225c31a3d86336fdd1eaa0
CoolConorCoder <presaltishere@gmail.com>
2024-11-27 14:46:55 -0500

    Add files via upload

---
+++ coolWebsite/Ask.js
@@ -0,0 +1,13 @@
+function changeBackground () {;
+    document.body.style.background = ('purple');
+}   
+function website (){;
+    location.href = "https://youtube.com";
+}
+function changeHello (){;
+    document.getElementById("alex").innerText = "hello world";
+}
+function nothing (){;
+    alert("lil bro u gonna reget this")
+    location.href = "   https://www.youtube.com/watch?v=r8E0CbYZcjE";
+}
